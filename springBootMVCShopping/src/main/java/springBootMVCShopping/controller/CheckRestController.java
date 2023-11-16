813d54a72de9c0d03e8e3cc58a3c1b375e9735a5
hk002 <hk002@DESKTOP-R9I9TP1>
2023-11-16 18:01:01 +0900

    상품이미지 변경하기

--- springBootMVCShopping/src/main/java/springBootMVCShopping/controller/CheckRestController.java
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/controller/CheckRestController.java
@@ -1,12 +1,16 @@
 package springBootMVCShopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
+import springBootMVCShopping.command.FileCommand;
 import springBootMVCShopping.service.EmailCheckService;
+import springBootMVCShopping.service.FileDelService;
 import springBootMVCShopping.service.UserEmailCheckService;
 
 // 같은 방법 다른 형식으로 @ResponseBody를 사용했다면 @RestController를 사용하겠습니다.
@@ -32,6 +36,14 @@ public class CheckRestController {
 		if(i == 0) 
 			return "이미 인증되었습니다. ";
 		else	return "인증되었습니다.";
+	}
+	// RestAPI를 이용하여 파일 삭제할 정보를 저장합니다/.
+	// 먼저 ajax로부터 넘어온 삭제할 파일의 정보를  FileCommand에 저장합니다.
+	@Autowired
+	FileDelService fileDelService;
+	@PostMapping("goods/fileDel")
+	public String fileDel(FileCommand fileCommand, HttpSession session) {
+		return fileDelService.execute(fileCommand, session);
 	}
 }
 
